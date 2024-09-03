d4bc723059b8abcc04b5702055ed8f6444e6afc9
gabriel faria <gabrielfariasilva10@hotmail.com>
2024-09-03 16:07:18 -0300

    feat: view2 updated

--- view2.sql
+++ view2.sql
@@ -1,14 +1,49 @@
 CREATE OR REPLACE VIEW `sapient-cycling-434419-u0.Campaigns.v_interacoes_campanhas` AS
-WITH location_ranking AS (
-  SELECT 
-    client_id,
-    MAX(type_campaign) AS most_campaigns,
-    COUNTIF(return_status = 'error') AS quantity_error,
-    FORMAT_DATE('%Y-%m-%d', CURRENT_DATE()) AS date_today,
-    CAST(FORMAT_DATE('%m%Y', CURRENT_DATE()) AS INT64) AS anomes_today
-  FROM 
-    `sapient-cycling-434419-u0.Campaigns.Campaigns`
-  GROUP BY 
-    client_id
+WITH received AS (
+    SELECT 
+        client_id,
+        id_campaign, 
+        COUNT(*) AS times_received, 
+        ROW_NUMBER() OVER (PARTITION BY client_id ORDER BY COUNT(*) DESC) AS most_received 
+    FROM 
+        `sapient-cycling-434419-u0.Campaigns.Campaigns` 
+    WHERE 
+        return_status = "received" 
+    GROUP BY 
+        client_id, 
+        id_campaign
+),
+
+error_quantities AS (
+    SELECT 
+        client_id, 
+        COUNT(*) AS quantity_error 
+    FROM 
+        `sapient-cycling-434419-u0.Campaigns.Campaigns` 
+    WHERE 
+        return_status = "error" 
+    GROUP BY 
+        client_id
 )
-SELECT * FROM location_ranking;
+
+SELECT
+    r.client_id AS client_id,
+    r.id_campaign AS most_campaign,
+    e.quantity_error AS quantity_error,
+    CURRENT_DATE() AS date_today,
+    CAST(FORMAT_DATE('%m%Y', CURRENT_DATE()) AS INT64) AS anomes_today
+FROM
+    received r
+JOIN
+    error_quantities e
+ON
+    r.client_id = e.client_id AND
+    r.most_received = 1
+GROUP BY
+    r.client_id,
+    r.id_campaign,
+    e.quantity_error
+ORDER BY
+    r.client_id;
+
+SELECT * FROM `sapient-cycling-434419-u0.Campaigns.v_interacoes_campanhas` LIMIT 10
